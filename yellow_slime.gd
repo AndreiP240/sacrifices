7b13fd1a7791f7f63eec1d98db1d919b49754605
AndreiP240 <116425904+AndreiP240@users.noreply.github.com>
2025-09-27 16:19:18 +0300

    Slime movement

---
+++ yellow_slime.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+@export var leader_path: NodePath  # drag the player node here in the editor
+@export var delay_frames := 40     # ~1 second delay (depends on FPS)
+
+var leader: Node = null
+
+func _ready():
+	leader = get_node(leader_path)
+
+func _physics_process(delta):
+	if leader == null:
+		return
+
+	var history = leader.position_history
+	if history.size() > delay_frames:
+		global_position = history[history.size() - delay_frames]
