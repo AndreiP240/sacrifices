0c60a4c64cdcbfb7950f955977f32f289b53fb85
AndreiP240 <116425904+AndreiP240@users.noreply.github.com>
2025-09-27 21:12:50 +0300

    Code fix

--- movement.gd
+++ movement.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 const SPEED = 500.0
 const JUMP_VELOCITY = -500
 var position_history: Array[Vector2] = []
+var PlatformScene = preload("res://red_platform.tscn")
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -17,3 +18,13 @@ func _physics_process(delta: float) -> void:
 	position_history.append(global_position)
 	if position_history.size() > 600:
 		position_history.pop_front()
+func _process(delta):
+	if Input.is_action_just_pressed("red_platform"):
+		summon_platform()
+func summon_platform():
+	var platform = PlatformScene.instantiate()
+	var offset = Vector2(0, 32)  # adjust depending on your player size
+	platform.position = global_position + offset
+	get_tree().current_scene.add_child(platform)
+	print("Platform spawned at:", platform.position)
+	print("Parent node:", get_parent())
