7870825be9350fa511e734c1497185914d28c407
AndreiP240 <116425904+AndreiP240@users.noreply.github.com>
2025-09-27 14:06:44 +0300

    test

---
+++ player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+const SPEED = 500.0
+const JUMP_VELOCITY = -400
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += JUMP_VELOCITY
+	var direction := Input.get_axis("left", "right") 
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(direction, 0, SPEED)
+	move_and_slide()
