ffa9db1b7c96744ab8162ebdb6cc30d521e867d3
AndreiP240 <116425904+AndreiP240@users.noreply.github.com>
2025-09-27 16:19:18 +0300

    Slime movement

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 const SPEED = 500.0
 const JUMP_VELOCITY = -400
+var position_history: Array[Vector2] = []
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -13,3 +14,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(direction, 0, SPEED)
 	move_and_slide()
+	position_history.append(global_position)
+	if position_history.size() > 600:
+		position_history.pop_front()
